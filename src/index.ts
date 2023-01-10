22b40df3df63c8cd8319006bbb92cfbfbb72cd5a
Joao-Neto-DH <joaolima882018@gmail.com>
2023-01-10 21:04:37 +0100

    Definindo rota dos games

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,30 @@ const app = Express();
 const prisma = new PrismaClient();
 
 // Jogos
-app.get("/games", (request, response)=> {
+app.get("/games", async (request, response)=> {
+    let games = (await prisma.game.findMany({
+        skip: 0,
+        take: 10
+    }));
+        // console.log(games.flatMap( game => ({...game,"link":"j"})));
 
+    response.status(200).send({
+        "content-type": "text/json",
+        "status": 200,
+        "count": games.length,
+        "page": 1,
+        "datas": games.flatMap( game => ({...game,"link": `/games/${game.id}`}) )
+    });
 });
 app.get("/games/:id", (request, response)=> {
     
 });
 
 // Comentários
-app.post("/comments", (request, response)=> {
+app.post("/games/:id/comments", (request, response)=> {
     
 });
-app.get("/comments/:id/responses", (request, response)=>{
+app.get("/games/:gameId/comments/:commentId/responses", (request, response)=>{
 
 });
 
