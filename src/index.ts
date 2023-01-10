13e990510f89740db2fd834757c9205fe662fcab
Joao-Neto-DH <joaolima882018@gmail.com>
2023-01-10 23:30:07 +0100

    Adicionando imagens do jogo a rota dos jogos

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,47 @@ const prisma = new PrismaClient();
 
 // Jogos
 app.get("/games", async (request, response)=> {
-    let games = (await prisma.game.findMany({
+    const games = (await prisma.game.findMany({
+        include: {
+            screenshoots:{
+                take: 1,
+                select: {
+                    image_path: true
+                },
+                where: {
+                    cover: true
+                }
+            }
+        },
         skip: 0,
         take: 10
     }));
-        // console.log(games.flatMap( game => ({...game,"link":"j"})));
 
     response.status(200).send({
         "content-type": "text/json",
         "status": 200,
         "count": games.length,
         "page": 1,
-        "datas": games.flatMap( game => ({...game,"link": `/games/${game.id}`}) )
+        "datas": games.flatMap( game => ({
+            "link": `/games/${game.id}`,
+            ...game
+        }) )
     });
 });
 
 app.get("/games/:id", async (request, response)=> {
     const game = await prisma.game.findFirst({
+        include: {
+            screenshoots: {
+                take: 1,
+                select: {
+                    image_path: true
+                },
+                where: {
+                    cover: true
+                }
+            }
+        },
         where: {
             id: request.params.id
         }
@@ -32,7 +56,7 @@ app.get("/games/:id", async (request, response)=> {
         return response.status(200).send({
             "content-type": "text/json",
             "status": 200,
-            "game": {...game, "link": `/games/${game.id}`}
+            "game": game
         });
     
     response.status(404).send({
@@ -43,8 +67,36 @@ app.get("/games/:id", async (request, response)=> {
 });
 
 // Comentários
-app.post("/games/:id/comments", (request, response)=> {
-    
+app.get("/games/:id/comments", async (request, response)=> {
+    const comments = await prisma.comment.findMany({
+        where: {
+            gameId: request.params.id
+        }
+    });
+
+    response.status(200).send({
+        "content-type": "text/json",
+        "status": 200,
+        "game_link": `/games/${request.params.id}`,
+        "count": comments.length,
+        "datas": comments
+    });
+});
+
+app.post("/games/:id/comments", async (request, response)=> {
+    // const comments = await prisma.comment.findMany({
+    //     where: {
+    //         gameId: request.params.id
+    //     }
+    // });
+
+    // response.status(200).send({
+    //     "content-type": "text/json",
+    //     "status": 200,
+    //     "game_link": `/games/${request.params.id}`,
+    //     "count": comments.length,
+    //     "comments": comments
+    // });
 });
 app.get("/games/:gameId/comments/:commentId/responses", (request, response)=>{
 
